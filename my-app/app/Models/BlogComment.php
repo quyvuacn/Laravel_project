3c4619ffae51d8f0edb57f9cff9d95333d111610
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-08-20 10:21:58 +0700

    quý

---
+++ my-app/app/Models/BlogComment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlogComment extends Model
+{
+    use HasFactory;
+    protected $table = 'blog_comments';
+    protected $primaryKey = 'id';
+    protected $guarded = [];
+}
